c5173ddfc34095703cd0c3ede0565b7fe5c41168
rabhijit <abhijit.ravichandran@gmail.com>
2019-08-24 17:11:24 +0800

    Level-3 complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,7 +8,7 @@ public class Duke {
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t____________________________________________________________");
 
-        ArrayList<String> items = new ArrayList<String>();
+        ArrayList<Task> items = new ArrayList<Task>();
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
@@ -20,16 +20,30 @@ public class Duke {
             }
             else if (input.equals("list")) {
                 System.out.println("\t____________________________________________________________");
+                System.out.println("\tHere are the tasks in your list:");
                 for (int i = 0; i < items.size(); i++) {
-                    System.out.println("\t" + (i+1) + ". " + items.get(i));
+                    Task thisTask = items.get(i);
+                    System.out.println("\t" + (i+1) + ". [" + thisTask.getStatusIcon() + "] " + thisTask.getDescription());
                 }
                 System.out.println("\t____________________________________________________________");
             }
             else {
-                System.out.println("\t____________________________________________________________");
-                System.out.println("\t" + "added: " + input);
-                System.out.println("\t____________________________________________________________");
-                items.add(input);
+                String[] words = input.split(" ");
+                if (input.contains("done") && words.length == 2 && words[1].matches("-?\\d+(\\.\\d+)?")) {
+                    int taskNumber = Integer.parseInt(words[1]) - 1;
+                    Task thisTask = items.get(taskNumber);
+                    thisTask.markAsDone();
+                    System.out.println("\t____________________________________________________________");
+                    System.out.println("\tNice! I've marked this task as done:");
+                    System.out.println("\t\t[" + thisTask.getStatusIcon() + "] " + thisTask.getDescription());
+                }
+                else {
+                    System.out.println("\t____________________________________________________________");
+                    System.out.println("\t" + "added: " + input);
+                    System.out.println("\t____________________________________________________________");
+                    Task newTask = new Task(input);
+                    items.add(newTask);
+                }
             }
         }
     }
